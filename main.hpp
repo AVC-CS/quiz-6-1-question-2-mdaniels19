ac30e4bc71bbc9bac4c5466156af74bddf29644e
Moth <catlovermittens344@gmail.com>
2025-04-25 16:06:41 -0700

    main done

--- main.hpp
+++ main.hpp
@@ -1,3 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//Employee ID: Integer
+//* Employee Name: String
+//* Department Name: String
+//* Salary: double
+
+int employid;
+string name;
+string departname;
+double salary;
+
+
+
 /***************************************************
  * Code your program here
  ***************************************************/
